33a4a80dbba307b5578ff2e4ef1a43edbf302f29
surajj <pandeyyyysuraj@gmail.com>
2023-05-22 03:14:44 +0530

    Create Readme

--- index.js
+++ index.js
@@ -4,109 +4,110 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 
-
 const app = express();
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 const port = process.env.PORT || 3000;
 
 // mongoose
-const connection = async ()=>{
-
-    try {
-        mongoose.connect(process.env.URI);
-        console.log("database connected");
-    } catch (error) {
-        console.log("error in database connection");
-    }
-}
-const URI = process.env.URI+"/user";
+const connection = async () => {
+  try {
+    mongoose.connect(process.env.URI);
+    console.log("database connected");
+  } catch (error) {
+    console.log("error in database connection");
+  }
+};
+const URI = process.env.URI + "/user";
 console.log(URI);
 
-
-mongoose.connect(process.env.URI)
+mongoose.connect(process.env.URI);
 
 const Schema = mongoose.Schema;
-const data = new Schema({
+const data = new Schema(
+  {
     messege: { type: String, required: true },
     date: { type: Date, default: Date.now },
-}, { versionKey: false });
+  },
+  { versionKey: false }
+);
 
-const item = mongoose.model('item',data);
+const item = mongoose.model("item", data);
 
-
-app.get("/", async(req, res) => {
-    const datas = await item.find();
-     res.render("loader",{data:datas});
+app.get("/", async (req, res) => {
+  const datas = await item.find();
+  res.render("loader", { data: datas });
 });
-
 
 app.post("/delete/:id", async (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    try {
-        const deletee = await item.deleteOne({ "_id": id });
-        if (deletee) {
-            console.log("deleted", id);
-            res.redirect("/");
-        } else {
-            console.log("not deleted");
-            res.send("<script>window.alert(`error in deleting`);window.location.href='/';</script>");
-        }
-    } catch (error) {
-        console.log("error");
-        res.sendStatus(404);
+  try {
+    const deletee = await item.deleteOne({ _id: id });
+    if (deletee) {
+      console.log("deleted", id);
+      res.redirect("/");
+    } else {
+      console.log("not deleted");
+      res.send(
+        "<script>window.alert(`error in deleting`);window.location.href='/';</script>"
+      );
     }
+  } catch (error) {
+    console.log("error");
+    res.sendStatus(404);
+  }
 });
-
 
 app.post("/update/:id", async (req, res) => {
-    const id = req.params.id;
-    const newText = req.body.editText;
+  const id = req.params.id;
+  const newText = req.body.editText;
 
-    try {
-        const updatedItem = await item.findByIdAndUpdate(id, { messege: newText });
-        if (updatedItem) {
-            console.log("updated", id);
-            res.redirect("/");
-        } else {
-            console.log("not updated");
-            res.send("<script>window.alert(`error in updating`);window.location.href='/';</script>");
-        }
-    } catch (error) {
-        console.log("error",error);
-        res.send("<script>window.alert(`error in updating`);window.location.href='/';</script>");
-        // res.sendStatus(404);
+  try {
+    const updatedItem = await item.findByIdAndUpdate(id, { messege: newText });
+    if (updatedItem) {
+      console.log("updated", id);
+      res.redirect("/");
+    } else {
+      console.log("not updated");
+      res.send(
+        "<script>window.alert(`error in updating`);window.location.href='/';</script>"
+      );
     }
+  } catch (error) {
+    console.log("error", error);
+    res.send(
+      "<script>window.alert(`error in updating`);window.location.href='/';</script>"
+    );
+    // res.sendStatus(404);
+  }
 });
 
+app.post("/upload", (req, res) => {
+  const task = new item({
+    messege: req.body.work,
+  });
+  console.log(task);
 
-
-
-
-app.post("/upload",(req,res)=>{
-    const task = new item({
-        messege : req.body.work
-    });
-    console.log(task);
-
-    try {
-       const s = task.save();
-       if(s){
-        console.log("data saved",s);
-        res.redirect("/");
-       }else{
-        console.log("error in saving");
-        req.redirect("/");
-       }
-       
-    } catch (error) {
-        console.log("error here ",error);
-        res.send(`<script>window.alert("error in saving");window.location.href="/";</script>`)
-    };
+  try {
+    const s = task.save();
+    if (s) {
+      console.log("data saved", s);
+      res.redirect("/");
+    } else {
+      console.log("error in saving");
+      req.redirect("/");
+    }
+  } catch (error) {
+    console.log("error here ", error);
+    res.send(
+      `<script>window.alert("error in saving");window.location.href="/";</script>`
+    );
+  }
 });
 
-
-app.listen(port, () => console.log(` app listening on port ${port}, http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(` app listening on port ${port}, http://localhost:${port}`)
+);
---
+++ readme.md
@@ -0,0 +1,59 @@
+## To Do list
+---
+It's a full stack,dynamic,and responsive website for all of the devices built on the server of node js,also used mongodb for database.
+
+
+
+---
+###setup
+
+clone the repo
+```bash
+git clone https://github.com/surajjbhardwaj/To-do-list
+```
+
+change the directory
+```bash
+cd To-do-list
+```
+
+now install the dependencies
+```bash
+npm install
+```
+
+create a dotenv file and set environment variables for mongodb uri and port
+
+PORT= your port 
+URI= your mongoose connection string
+
+### now you're ready to run on local server
+
+just run the command
+
+```bash
+npm start
+```
+
+now you can track your website on ```https://localhost:${process.env.PORT}```
+
+
+### why to use 
+
+very simple and perfectly built
+
+like a edit button for updating your work and delete button for delete your todo work
+
+---
+
+## how i implemented
+
+* For the backend i used node js and some popular libraries like mongoose,body-parser and ejs.
+
+* for frontend templeting i used ejs,because it allows us to run ```javascript``` code in html
+
+
+
+
+
+
