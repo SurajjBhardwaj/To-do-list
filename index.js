354985889c596f29792179f440350254ab84f117
surajj <pandeyyyysuraj@gmail.com>
2023-05-22 02:33:30 +0530

    final commit

--- index.js
+++ index.js
@@ -21,16 +21,18 @@ const connection = async ()=>{
     } catch (error) {
         console.log("error in database connection");
     }
-
-
 }
+const URI = process.env.URI+"/user";
+console.log(URI);
+
+
 mongoose.connect(process.env.URI)
 
 const Schema = mongoose.Schema;
 const data = new Schema({
-    messege:{type:String,required:true},
-    date:{type:Date,default:Date.now}
-})
+    messege: { type: String, required: true },
+    date: { type: Date, default: Date.now },
+}, { versionKey: false });
 
 const item = mongoose.model('item',data);
 
@@ -40,29 +42,47 @@ app.get("/", async(req, res) => {
      res.render("loader",{data:datas});
 });
 
-app.post("/delete",async(req,res)=>{
-    const id = req.body.checkbox;
-    console.log(id);
+
+app.post("/delete/:id", async (req, res) => {
+    const id = req.params.id;
 
     try {
-
-        const deletee = await item.deleteOne({"_id":id});
-        if(deletee) {
-            console.log("deleted ",id);
+        const deletee = await item.deleteOne({ "_id": id });
+        if (deletee) {
+            console.log("deleted", id);
             res.redirect("/");
-        }else{
+        } else {
             console.log("not deleted");
-            res.send("<script>window.alert(`error in saving`);window.location.href="/"</script>");
-
-
+            res.send("<script>window.alert(`error in deleting`);window.location.href='/';</script>");
         }
-        
     } catch (error) {
         console.log("error");
         res.sendStatus(404);
-        // res.send("<script>window.alert(`error in saving`);window.location.href="/"</script>")
     }
-})
+});
+
+
+app.post("/update/:id", async (req, res) => {
+    const id = req.params.id;
+    const newText = req.body.editText;
+
+    try {
+        const updatedItem = await item.findByIdAndUpdate(id, { messege: newText });
+        if (updatedItem) {
+            console.log("updated", id);
+            res.redirect("/");
+        } else {
+            console.log("not updated");
+            res.send("<script>window.alert(`error in updating`);window.location.href='/';</script>");
+        }
+    } catch (error) {
+        console.log("error",error);
+        res.send("<script>window.alert(`error in updating`);window.location.href='/';</script>");
+        // res.sendStatus(404);
+    }
+});
+
+
 
 
 
